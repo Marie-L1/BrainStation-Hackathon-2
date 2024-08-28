8134cca11a8106a739a524b2104167af45acea1f
Maria Lukowich <mlukowich27@gmail.com>
2024-08-28 15:01:22 -0600

    trying to get the prompt and image for the input page

--- hackathon-2/src/components/EntryPagePrompt/EntryPrompt.jsx
+++ hackathon-2/src/components/EntryPagePrompt/EntryPrompt.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import "./EntryPrompt.scss";
 
-function PromptEntry() {
+function PromptEntry({ selectedPrompt }) {
   return (
-    <section className="prompt-entry">
-        <img className="prompt-entry__img" 
-        src="#"
-        alt="" />
-        <h2 className="prompt-entry__title">{}</h2>
-    </section>
-  )
-}
+      <section className="prompt-entry">
+        <div className="prompt-entry__wrapper">
+          {/* <img
+            className="prompt-entry__img"
+            src={selectedPrompt.image}
+            alt={`Image of "${selectedPrompt.prompt}"`}
+          /> */}
+          {/* <h2 className="prompt-entry__title">{selectedPrompt.prompt}</h2> */}
+        </div>
+      </section>
+    );
+  }
 
 export default PromptEntry
