08f95f130f69fd06ea0649aab199374ccef9ba3c
rakshajay <rakshashettyhs@gmail.com>
2024-08-28 13:24:00 -0700

    promptslist linked to entry page

--- hackathon-2/src/App.jsx
+++ hackathon-2/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
+import EntryPage from './pages/EntryPage/EntryPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage /> }/>
           <Route path="/entry/:id" element="<HomePage />" />
+          <Route path="/entrypage" element={<EntryPage /> }/>
         </Routes>
       </BrowserRouter>
     </>
