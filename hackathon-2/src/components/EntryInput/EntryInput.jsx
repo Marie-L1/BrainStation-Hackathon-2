78219dc1786dd1410c12395b1f35b75dfc6cb5e0
Maria Lukowich <mlukowich27@gmail.com>
2024-08-28 13:09:05 -0600

    adding handleEntrySubmit

--- hackathon-2/src/components/EntryInput/EntryInput.jsx
+++ hackathon-2/src/components/EntryInput/EntryInput.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function EntryInput() {
+function EntryInput({handleEntrySubmit}) {
   return (
     <>
-        <form className="entry">
+        <form className="entry" onSubmit={handleEntrySubmit}>
             <input className="entry__title" id="entry-title" placeholder='Entry title'></input>
             <textarea className="entry__textarea" placeholder='Entry'></textarea>
 
-            <button type="submit" className="entry__btn">SUBMIT</button>
+            <button className="entry__btn">SUBMIT</button>
         </form>
     </>
   )
--- hackathon-2/src/pages/EntryPage/EntryPage.jsx
+++ hackathon-2/src/pages/EntryPage/EntryPage.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './EntryPage.scss'
+import axios from 'axios';
 import EntryPrompt from '../../components/EntryPagePrompt/EntryPrompt';
 import EntryInput from '../../components/EntryInput/EntryInput';
 
 
 function EntryPage() {
+  const baseUrl = import.meta.env.VITE_API_URL;
+
+  const handleEntrySubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post(`${baseUrl}/entries`, {
+      title: event.target.title.value,
+      entry: event.target.entry.value
+    });
+    console.log(response)
+  }
+
   return (
     <>
         <EntryPrompt />
-        <EntryInput />
+        <EntryInput handleEntrySubmit={handleEntrySubmit}/>
     </>
   )
 }
