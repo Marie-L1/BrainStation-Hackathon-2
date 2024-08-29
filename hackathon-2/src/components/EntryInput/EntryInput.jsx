68e94d832f268f36dfe5df8d8e68c3a9290c94b6
rakshajay <rakshashettyhs@gmail.com>
2024-08-28 18:16:43 -0700

    Final push before next final :)

--- hackathon-2/src/components/EntryInput/EntryInput.jsx
+++ hackathon-2/src/components/EntryInput/EntryInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './EntryInput.scss'
 
 function EntryInput({handleEntrySubmit}) {
   return (
--- hackathon-2/src/components/Header/Header.jsx
+++ hackathon-2/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Header.scss'
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png'
 
 function Header() {
   return (
     <div className="header">
+      <img src={logo} alt="logo" className="header__title-img"/>
       <h1 className="header__title">Your Personal Journal</h1>
-
+      
       <div className="header__nav">
       <Link to="/" id="homelink"><h2 className="header__nav-home">Home</h2></Link>
         <h2 className="header__nav-entries">Past Entries</h2>
